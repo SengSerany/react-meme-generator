a466a69e2b813e4bc0d3e6c78b688f2b1543cd87
SengSerany <cranyseng@gmail.com>
2022-04-13 11:20:09 +0200

    change img when clicking on the btn

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,26 @@
 import React from "react";
+import MemeData from "../memeData";
 
 export default function Meme() {
+
+    const [ memeImage, getMemeImage] = React.useState("https://static.actu.fr/uploads/2021/04/img-0404-960x640.jpg");
+
+    const newMemeImg = () => {
+        let memeArray = MemeData.data.memes;
+        let randomNum = Math.floor(Math.random() * (memeArray.length - 1));
+        return getMemeImage(memeArray[randomNum].url) 
+    }
+
     return (
-        <form>
-            <div className="meme--inputs">
-                <input type="text" value="Shut up" className="meme--input"/>
-                <input type="text" value="and take my money" className="meme--input"/>
-            </div>
-            <input type="submit" value="Get a new meme image 🖼" className="meme--submit" />
-        </form>
+        <div className="main">
+            <form>
+                <div className="meme--inputs">
+                    <input type="text" placeholder="Top text" className="meme--input"/>
+                    <input type="text" placeholder="Bottom text" className="meme--input"/>
+                </div>
+                <button type="button" onClick={newMemeImg} className="meme--submit">Get a new meme image 🖼</button>
+            </form>
+            <img src={memeImage} alt="meme representation" className="meme--img" />
+        </div>
     )
 }
